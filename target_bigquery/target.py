8dc5ad0a7547f0740a99b90fb8a37e785c6dffa1
moses <60942157+vemqar@users.noreply.github.com>
2025-09-24 13:09:17 -0400

    fix validate

--- target_bigquery/target.py
+++ target_bigquery/target.py
@@ -590,7 +590,7 @@ class TargetBigQuery(Target):
         self, raise_errors: bool = True, warnings_as_errors: bool = False
     ) -> Tuple[List[str], List[str]]:
         """Don't throw on config validation since our JSON schema doesn't seem to play well with meltano for whatever reason"""
-        return super()._validate_config(False, False)
+        return super()._validate_config()
 
 
 if __name__ == "__main__":
